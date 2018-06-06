17ace33f3ceb86a1df1e470c7267a827b47ad692
ivanljt <596476418@qq.com>
2018-06-07 00:27:33 +0800

    add implementation of reverse linkedlist

--- src/t06逆序打印链表/PrintLinkedListInReverse.java
+++ src/t06逆序打印链表/PrintLinkedListInReverse.java
@@ -14,9 +14,20 @@ class ListNode {
 
 /**
  * 输入一个链表，从尾到头打印链表每个节点的值。
+ *
+ * 思路：逆序 —> 可以借助数据结构：栈
+ * —>或者是  List 的 add 方法
+ *
+ * 不要一开始考虑 链表自身的逆转。
+ *
+ * 其实返回值如果是链表的话。可能会更合适一点
  */
 class PrintLinkedListInReverse {
 
+    /**
+     * 时间 O()
+     * 空间 O()
+     * */
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         while (listNode != null) {
@@ -25,4 +36,6 @@ class PrintLinkedListInReverse {
         }
         return list;
     }
+
+
 }
