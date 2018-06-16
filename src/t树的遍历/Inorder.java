a79d4d397d61dfdfa463d67a8ef5408c86663d90
ivanljt <596476418@qq.com>
2018-06-16 13:56:02 +0800

    add preorder and inorder traversal impl

--- src/t树的遍历/Inorder.java
+++ src/t树的遍历/Inorder.java
@@ -1,5 +1,7 @@
 package t树的遍历;
 
+import java.util.Stack;
+
 /**
  * 先打印根节点，再打印左子树，最后打印右子树
  */
@@ -17,10 +19,27 @@ class Inorder {
     }
 
     /**
-     * 迭代遍历
+     * 迭代实现中序遍历。打印顺序:左子树  根节点  右子树
+     * 1. 申请一个栈 stack，申请一个变量，初始时指向头节点
+     * 2. 先把 current 压入栈中,对以cur节点为头的整棵子树来说，依次把整棵树的左边界压入栈中，也就是不断地令 cur = cur.left
+     * 3. 不断重复2，直到发现 cur 为空，此时从 stack 中弹出一个节点，记为 node，打印 node的值，并让 cur = node.right，然后重复步骤 2
+     * 4. stack 为空 & cur 为空时，整个过程结束
      */
     void inorderIter(TreeNode pRoot) {
+        if (pRoot == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || pRoot != null) {//Stack 有两个判空方法，empty（自身提供）、isEmpty（从 vector 继承而来）
 
+            while (pRoot != null) {//不断地将左子节点放入栈中，该循环结束时，栈顶部为「最左的叶子节点」
+                stack.push(pRoot);
+                pRoot = pRoot.left;
+            }
+            //pRoot 为 null 时,说明之前 已经到了「最左的叶子节点」
+            pRoot = stack.pop();//弹出节点
+            System.out.println(pRoot.val);//打印
+            pRoot = pRoot.right;//指向当前节点的右子树（重复步骤2）
+        }
     }
-
 }
--- src/t树的遍历/Preorder.java
+++ src/t树的遍历/Preorder.java
@@ -1,11 +1,14 @@
 package t树的遍历;
 
+import java.util.Currency;
+import java.util.Stack;
+
 class Preorder {
     /**
      * 递归遍历
      */
     void preorderRecursive(TreeNode pRoot) {
-        if (pRoot == null){
+        if (pRoot == null) {
             return;
         }
         System.out.println(pRoot.val);
@@ -14,10 +17,27 @@ class Preorder {
     }
 
     /**
-     * 迭代遍历
+     * 迭代方式进行中序遍历。
+     * 初始状态下，将根节点添加到栈中
+     * 每一次循环中，打印一个节点之后 把该节点的「右子树的根节点」以及 「左子树的根节点」放到栈中
      */
-    void preorderIter() {
-
+    void preorderIter(TreeNode pRoot) {
+        if (pRoot == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(pRoot);
+        TreeNode cur;
+        while (!stack.empty()) {
+            cur = stack.pop();
+            System.out.println(cur);
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
     }
 
 }
