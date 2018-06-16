d4e9d344954dda9045c0d80c3873767aba964d57
ivanljt <596476418@qq.com>
2018-06-16 17:15:55 +0800

    add non recursive postorder travelsal impl

--- src/t54二叉树的第k个节点/KNode.java
+++ src/t54二叉树的第k个节点/KNode.java
@@ -1,9 +1,9 @@
-package t54二叉树第k大;
+package t54二叉树的第k个节点;
 
 import java.util.Stack;
 
 class TreeNode {
-    int val = 0;
+    int val;
     TreeNode left = null;
     TreeNode right = null;
 
@@ -14,9 +14,9 @@ class TreeNode {
 
 }
 
-class KNode {
+public class KNode {
 
-    int count = 0;
+    private int count = 0;
 
     /**
      * 迭代方式。使用栈的解法。
@@ -40,6 +40,32 @@ class KNode {
                     return node;
                 }
                 pRoot = node.right;
+            }
+        }
+        return null;
+    }
+
+    //题目应该是「第 k 个」而不是「第 k 大」
+    //中序遍历的第 k 个节点，
+    TreeNode KthNode1(TreeNode pRoot, int k) {
+        if (pRoot == null || k == 0) {
+            return null;
+        }
+        //迭代实现,每一次循环中，删除特定的节点
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = pRoot;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (!stack.isEmpty()) {
+                cur = stack.pop();
+                k--;
+                if (k == 0) {
+                    return cur;
+                }
+                cur = cur.right;
             }
         }
         return null;
--- src/t树的遍历/Postorder.java
+++ src/t树的遍历/Postorder.java
@@ -1,8 +1,10 @@
 package t树的遍历;
 
+import java.util.Stack;
+
 class Postorder {
     /**
-     * 递归遍历
+     * 递归遍历方式
      */
     void postorderRecursive(TreeNode pRoot) {
         if (pRoot == null) {
@@ -14,10 +16,35 @@ class Postorder {
     }
 
     /**
-     * 迭代遍历
+     * 双栈实现非递归后序遍历，
+     * 1. 初始化两个栈，stack1，stack2. 先将根节点放入 stack1 中，
+     * 2. 从 stack1 中弹出栈顶节点 node， 将 node 放入 stack2 中，
+         * 如果 node 的左子节点不为空，将之放入 stack1 中，（因为后面会放到 stack2 中，而栈是先进后出的。 在 stack1 中先进后出 —> 进入到 stack2 中会提前打印出来）
+         * 如果 node 的右子节点不为空，将之放入 Stack1 中
+     * 3. 重复步骤2，一直到 stack1 为空，此时所有的节点都已经在 stack2 中，按序打印 stack2 中的节点，即可得到后序遍历序列
      */
-    void postorderIter() {
+    void postorderIter(TreeNode pRoot) {
+        if (pRoot == null) {
+            return;
+        }
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
 
+        stack1.push(pRoot);
+        while (!stack1.isEmpty()) {
+            //每一个循环中，stack1中的一个节点node被弹出并添加到 stack2 中，  node的左右孩子被添加到了  stack1 中。（如果右孩子不为空，那么下一轮处理的就是 node 的右孩子）
+            pRoot = stack1.pop();
+            stack2.push(pRoot);
+            if (pRoot.left != null) {
+                stack1.push(pRoot.left);
+            }
+            if (pRoot.right != null) {
+                stack1.push(pRoot.right);
+            }
+        }
+        while (!stack2.isEmpty()) {
+            System.out.println(stack2.pop().val);
+        }
     }
 
 }
