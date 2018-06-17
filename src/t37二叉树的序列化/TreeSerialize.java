df9a559a69d417e858a070e7cc0bd5d2a346b450
ivanljt <596476418@qq.com>
2018-06-17 17:22:01 +0800

    add serialize problem description and solution annotation

--- src/t37二叉树的序列化/TreeSerialize.java
+++ src/t37二叉树的序列化/TreeSerialize.java
@@ -8,36 +8,74 @@ class TreeNode {
     public TreeNode(int val) {
         this.val = val;
     }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
 }
 
+/**
+ * 题目：请实现两个函数，分别用来序列化和反序列化二叉树
+ */
 public class TreeSerialize {
-    String Serialize(TreeNode root) {
+    /**
+     * 序列化二叉树。按照前序遍历的顺序进行序列化，每一个节点之间使用「，」分开，空节点使用「#」表示。
+     * <p>
+     * * 时间复杂度 O()。
+     * * 空间复杂度 O()。「递归调用栈」
+     */
+    String serialize(TreeNode root) {
         if (root == null) {
             return "#,";
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(root.val).append(",");
-        sb.append(Serialize(root.left));
-        sb.append(Serialize(root.right));
-        return sb.toString();
+        return String.valueOf(root.val) + "," +
+                serialize(root.left) +
+                serialize(root.right);
     }
 
-    int index = -1;//当前反序列化的节点
+    private int index = -1;//当前反序列化的节点
 
-    //反序列化
-    //每一轮中，确定好一个节点
-    TreeNode Deserialize(String str) {
+    /**
+     * 反序列化。
+     *
+     * 序列化的时候，使用「，」分隔元素，先使用 String 的 split 函数将字符串拆为数组。
+     * 每一轮完成一个节点的反序列化（包括节点的值和左右节点）。
+     * * 时间复杂度 O()
+     * * 空间复杂度 O()
+     */
+    TreeNode deserialize(String str) {
         index++;//当前序列化的节点
         if (index >= str.length()) {//边界检查
             return null;
         }
         String[] strAry = str.split(",");
         TreeNode node = null;
-        if (!strAry[index].equals("#")) {
+        if (!strAry[index].equals("#")) {//不是「#」，说明节点不为 null
             node = new TreeNode(Integer.parseInt(strAry[index]));
-            node.left = Deserialize(str);
-            node.right = Deserialize(str);
+            node.left = deserialize(str);
+            node.right = deserialize(str);
         }
         return node;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(10);
+        TreeNode left = new TreeNode(5);
+        TreeNode right = new TreeNode(15);
+
+        treeNode.left = left;
+        treeNode.right = right;
+        TreeSerialize treeSerialize = new TreeSerialize();
+        String serializeStr = treeSerialize.serialize(treeNode);
+        System.out.println(serializeStr);
+
+        TreeNode root = treeSerialize.deserialize(serializeStr);
+        System.out.println(root);
+        assert root.val == 10;
     }
 }
