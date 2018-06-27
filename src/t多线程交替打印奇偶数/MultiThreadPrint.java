d18ce432f16470db8774535b94dea32e0cbd7b73
ivanljt <596476418@qq.com>
2018-06-27 16:48:59 +0800

    add LRUcache simple impl modify mutilthread print impl

--- src/t多线程交替打印奇偶数/MultiThreadPrint.java
+++ src/t多线程交替打印奇偶数/MultiThreadPrint.java
@@ -1,5 +1,7 @@
 package t多线程交替打印奇偶数;
 
+import java.util.concurrent.Executor;
+
 /**
  * 题目:2个线程交替打印1-100内数字,其中t1线程打印奇数,t2线程打印偶数
  * 主要考察对多线程创建以及多线程执行顺序的应用,难点是通过对一个对象的加锁,避免多线程随机打印,用一个开关控制打印奇数还是偶数
@@ -56,9 +58,8 @@ class MultiThreadPrint {
  * 关键：同一把锁，一个处于 wait 状态，另一个处于 notify 状态。交替执行
  */
 class BySynchronize {
-    private int mNum;
+    private int mNum = 1;
     private boolean mIsOdd = true;//是否是奇数。从1 开始,所以是 true
-
 
     public static void main(String[] args) {
         BySynchronize bySynchronize = new BySynchronize();
@@ -78,13 +79,16 @@ class BySynchronize {
 
         @Override
         public void run() {
-            while (mBySynchronize.mNum <= 100) {//循环
+            while (mBySynchronize.mNum <= 99) {//循环
                 synchronized (mBySynchronize) {
                     if (mBySynchronize.mIsOdd) {//是奇数
                         System.out.println("ThreadOdd " + mBySynchronize.mNum++);
                         mBySynchronize.mIsOdd = false;
                     }
                     mBySynchronize.notify();
+                    if (mBySynchronize.mNum == 99) {
+                        break;
+                    }
                     try {
                         mBySynchronize.wait();
                     } catch (InterruptedException e) {
@@ -105,13 +109,15 @@ class BySynchronize {
         @Override
         public void run() {
             while (mBySynchronize.mNum <= 100) {
-
                 synchronized (mBySynchronize) {
                     if (!mBySynchronize.mIsOdd) {//是偶数
                         System.out.println("ThreadEven --- " + mBySynchronize.mNum++);//是偶数
                         mBySynchronize.mIsOdd = true;//
                     }
                     mBySynchronize.notify();
+                    if (mBySynchronize.mNum == 100) {
+                        break;
+                    }
                     try {
                         mBySynchronize.wait();
                     } catch (InterruptedException e) {
