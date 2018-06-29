197967f5c9fa4dc26fddc903f33abb5672ca17d6
ivanljt <596476418@qq.com>
2018-06-29 20:46:03 +0800

    add jvm heapOOM MetaSpaceOOM SOF sample

---
+++ src/memory_error_sample/VirtualStackOverFlow.java
@@ -0,0 +1,28 @@
+package memory_error_sample;
+
+class VirtualStackOverFlow {
+    int mStackDepth;
+
+    /**
+     * -Xss参数减少栈内存的容量
+     * -Xss128k
+     */
+    public static void main(String[] args) {
+        VirtualStackOverFlow virtualStackOverFlow = new VirtualStackOverFlow();
+        virtualStackOverFlow.sof();
+    }
+
+    void sof() {
+        try {
+            recursiveCall();
+        } catch (StackOverflowError e) {
+            System.out.println("stack depth:" + mStackDepth);
+            throw e;
+        }
+    }
+
+    private void recursiveCall() {
+        mStackDepth++;
+        recursiveCall();
+    }
+}
