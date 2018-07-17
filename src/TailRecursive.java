94dd7b7743c381977b4d53541fe753d8378e20af
ivanljt <596476418@qq.com>
2018-07-17 11:18:24 +0800

    add string permutation impl

--- src/TailRecursive.java
+++ src/TailRecursive.java
@@ -1,7 +1,13 @@
 
 /**
  * 尾递归。
- * 在结尾调用自身。
+ * 尾递归的实现，往往需要改写递归函数，确保最后一步只调用自身。做到这一点的方法，
+ * 就是把所有用到的内部变量改写成函数的参数。比如上面的例子，阶乘函数 factorial 需要用到一个中间变量 total ，
+ * 那就把这个中间变量改写成函数的参数。
+ * 这样做的缺点就是不太直观，第一眼很难看出来，为什么计算5的阶乘，需要传入两个参数5和1？
+ * 两个方法可以解决这个问题。
+ * 方法一，是在尾递归函数之外，再提供一个正常形式的函数。
+ * 方法二，如果使用的语言支持默认的参数值的话，将默认值设置为1
  */
 public class TailRecursive {
     /**
@@ -25,6 +31,8 @@ public class TailRecursive {
         }
         return n * nonTailRecursive(n - 1);//非尾递归
     }
+
+
 
     public static void main(String[] args) {
         TailRecursive tailRecursive = new TailRecursive();
