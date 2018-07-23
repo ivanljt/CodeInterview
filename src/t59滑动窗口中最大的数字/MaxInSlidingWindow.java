cc54d04377cdba2b23c56aa1e84fc0825527965e
ivanljt <596476418@qq.com>
2018-07-23 11:10:39 +0800

    add MaxNumberInSlidingWindow optimized impl

--- src/t59滑动窗口中最大的数字/MaxInSlidingWindow.java
+++ src/t59滑动窗口中最大的数字/MaxInSlidingWindow.java
@@ -1,6 +1,7 @@
 package t59滑动窗口中最大的数字;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。
@@ -18,10 +19,43 @@ import java.util.ArrayList;
 class MaxInSlidingWindow {
 
     /**
-     * 思路:使用一个队列（用其他的容器行不？）队列的头部存储最大值的下标，检查它 是否过期，
-     * 如果过期了，需要更新。 然后当前窗口最大值就是queue#peek
-     * */
+     * 思路:使用一个队列（用其他的容器行不？可以的，一维线性的内容都 ok）
+     * 队列的头部存储最大值的下标，
+     * 每一趟循环中检查它是否过期，如果过期了，需要更新。然后当前窗口最大值就是 queue#peekFirst
+     * <p>
+     * 假设窗口大小为 size。从左到右遍历数组，
+     * 判断过期的依据：queue#first < i -size + 1；
+     * 判断是否添加到结果中的依据是：i >= size -1
+     */
 
+    public ArrayList<Integer> maxInWindows(int[] num, int size) {
+        ArrayList<Integer> result = new ArrayList<>();
+        //[10,14,12,11],0
+        if (num == null || size <= 0 || size > num.length) {//size <=0 或者
+            return result;
+        }
+        LinkedList<Integer> queue = new LinkedList<>();//存储值在数组中的下标。
+        //依次遍历所有滑动窗口。
+        for (int i = 0; i < num.length; i++) {
+            while (!queue.isEmpty() && num[queue.getLast()] < num[i]) {//移除掉比当前元素小的数字
+                queue.removeLast();
+            }
+            //添加当前元素
+            queue.offerLast(i);//加进去如果发现太小了也无妨。因为后面会被移除掉
+            //判断队首下标是否过期
+            if (queue.getFirst() < i - size + 1) {
+                queue.removeFirst();
+            }
+            if (i >= size - 1) {//添加到结果列表中
+                result.add(num[queue.getFirst()]);
+            }
+        }
+        return result;
+    }
+
+}
+
+class SimpleImpl {
 
     /**
      * 直观的思路：遍历数组所有的滑动窗口。分别获取其中的最大值，加入到结果中。
