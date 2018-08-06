7e0b07e976e89511848b599942d0ce8b9c61bdaa
ivanljt <596476418@qq.com>
2018-08-06 23:58:25 +0800

    add FirstNotRepeatingCharInStream impl

--- src/t50_2字符流中第一个不重复的字符/FirstNotRepeatingCharInStream.java
+++ src/t50_2字符流中第一个不重复的字符/FirstNotRepeatingCharInStream.java
@@ -5,15 +5,11 @@ package t50_2字符流中第一个不重复的字符;
  */
 class FirstNotRepeatingCharInStream {
 
-}
-
-class SolutionRecord {
-
     //不需要一个额外的 StringBuilder 或者其他容器存储。一个长度为256的数组搞定。
     private int[] mOccurrence = new int[256];//下标为字符，值为**出现的位置对应的下标**，没有出现过的值 =1，出现>1次的，=-2
     private int mIndex = 0;//记录下标
 
-    public SolutionRecord() {
+    public FirstNotRepeatingCharInStream() {
         for (int i = 0; i < mOccurrence.length; i++) {
             mOccurrence[i] = -1;
         }
@@ -31,7 +27,7 @@ class SolutionRecord {
         mIndex++;
     }
 
-    //return the first appearence once char in current stringstream
+    //return the first appearance once char in current stringstream
     public char FirstAppearingOnce(String data) {
         if (data == null) {
             throw new IllegalArgumentException(data);
@@ -41,7 +37,7 @@ class SolutionRecord {
         }
         char ch = '#';//字符
         int minIndex = Integer.MAX_VALUE;
-        for (int i = 0; i < mOccurrence.length; i++) {
+        for (int i = 0; i < mOccurrence.length; i++) {//从头到尾遍历容器数组。
             if (mOccurrence[i] >= 0 && mOccurrence[i] < minIndex) {//只出现了一次
                 minIndex = mOccurrence[i];
                 ch = (char) i;//「数组下标」对应的值。也就是「出现了一次的值」
@@ -53,20 +49,20 @@ class SolutionRecord {
     }
 
     public static void main(String[] args) {
-        System.out.println(new SolutionRecord().FirstAppearingOnce("")); // '\0'
-        System.out.println(new SolutionRecord().FirstAppearingOnce("g")); // 'g'
-        System.out.println(new SolutionRecord().FirstAppearingOnce("go")); // 'g'
-        System.out.println(new SolutionRecord().FirstAppearingOnce("goo")); // 'g'
-        System.out.println(new SolutionRecord().FirstAppearingOnce("goog")); // '\0'
-        System.out.println(new SolutionRecord().FirstAppearingOnce("googl")); // l
-        System.out.println(new SolutionRecord().FirstAppearingOnce("googlea0")); // l
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("")); // '\0'
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("g")); // 'g'
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("go")); // 'g'
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("goo")); // 'g'
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("goog")); // '\0'
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("googl")); // l
+        System.out.println(new FirstNotRepeatingCharInStream().FirstAppearingOnce("googlea0")); // l
     }
 }
 
 class Sol2 {
-    int[] table = new int[256];
+    private int[] table = new int[256];
     //利用一个动态的数组记录插入的顺序，才能得到最先的顺序。StringBuilder 或者 容器都可以
-    StringBuilder sb = new StringBuilder();
+    private StringBuilder sb = new StringBuilder();
 
     //Insert one char from stringstream
     public void Insert(char ch) {
@@ -76,7 +72,7 @@ class Sol2 {
 
     //return the first appearence once char in current stringstream
     public char FirstAppearingOnce() {
-        for (int i = 0; i < sb.length(); i++) {
+        for (int i = 0; i < sb.length(); i++) {//按照字符输入顺序遍历数组。
             if (table[sb.charAt(i)] == 1) {
                 return sb.charAt(i);
             }
