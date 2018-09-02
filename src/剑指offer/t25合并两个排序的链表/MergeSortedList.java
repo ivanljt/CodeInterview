36f0d9af11bc150715686fc78ada98a86297de0e
ivanljt <596476418@qq.com>
2018-09-02 13:18:04 +0800

    impl merge two sorted linkedlist

---
+++ src/剑指offer/t25合并两个排序的链表/MergeSortedList.java
@@ -0,0 +1,31 @@
+package 剑指offer.t25合并两个排序的链表;
+
+import base.ListNode;
+/**
+ * 输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+ * */
+class MergeSortedList {
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                p.next = list1;
+                list1 = list1.next;
+            } else {
+                p.next = list2;
+                list2 = list2.next;
+            }
+            p = p.next;
+        }
+        p.next = list1 != null ? list1 : list2;
+        return dummy.next;
+    }
+
+}
